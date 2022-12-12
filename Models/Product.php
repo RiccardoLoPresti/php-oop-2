d0e546a23e6419ab7ae246356c405028bf813e15
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-12-12 13:57:14 +0100

    [add] category

--- Models/Product.php
+++ Models/Product.php
@@ -5,17 +5,17 @@ class Product{
     public $id;
     public $name;
     public $brand;
-    public $category;
+    public $categorys;
     public $price;
     public $is_avaible = true;
     public $image;
 
-    public function __construct($_id, $_name, $_brand, $_category, $_price, $_image)
+    public function __construct($_id, $_name, $_brand, Category $_categorys, $_price, $_image)
     {
         $this->id = $_id;
         $this->name = $_name;
         $this->brand = $_brand;
-        $this->category = $_category;
+        $this->categorys = $_categorys;
         $this->price = $_price;
         $this->image = $_image;
     }
--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 
 require_once __DIR__ . './Models/Product.php';
+require_once __DIR__ . './Models/Category.php';
+require_once __DIR__ . './Models/Food.php';
+require_once __DIR__ . './Models/Gadget.php';
+require_once __DIR__ . './Models/Toy.php';
 
-$product1 = new Product(12150,'mangiare', 'animalix','cani',30, 'https://medias.ultrapremiumdirect.com/italy/production/catalog/products/001005/1.jpg?tr=w-496,h-496&v=2118014580');
+$product1 = new Product(12150,'mangiare', 'animalix',new Category('cani','icona'),30, 'https://medias.ultrapremiumdirect.com/italy/production/catalog/products/001005/1.jpg?tr=w-496,h-496&v=2118014580');
 
 var_dump($product1);
 
