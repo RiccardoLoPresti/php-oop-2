a23a3b871cdfd9c689d01386dc7591c7c5529857
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-12-12 14:23:47 +0100

    [add] funcition for icon

--- Models/Category.php
+++ Models/Category.php
@@ -9,4 +9,16 @@ class Category{
         $this->name = $_name;
         $this->icon = $_icon;
     }
+
+    public function setIcon(){
+        if($this->name == 'cani'){
+            return $this->icon = 'https://img.freepik.com/free-vector/cute-dog-sticking-her-tongue-out-cartoon-icon-illustration_138676-2709.jpg?w=2000';
+        }else if($this->name == 'gatti'){
+            return $this->icon = 'https://img.freepik.com/free-vector/cute-cat-unicorn-play-box-cartoon-icon-illustration_138676-2645.jpg?w=2000';
+        }
+    }
+
+    public function getIcon(){
+        return $this->icon;
+    }
 }
--- index.php
+++ index.php
@@ -6,11 +6,20 @@ require_once __DIR__ . './Models/Food.php';
 require_once __DIR__ . './Models/Gadget.php';
 require_once __DIR__ . './Models/Toy.php';
 
-$product1 = new Product(12150,'mangiare', 'animalix',new Category('cani','icona'),30, 'https://medias.ultrapremiumdirect.com/italy/production/catalog/products/001005/1.jpg?tr=w-496,h-496&v=2118014580');
+$product1 = new Product(12150,'mangiare', 'animalix',new Category('cani','icon'),30, 'https://medias.ultrapremiumdirect.com/italy/production/catalog/products/001005/1.jpg?tr=w-496,h-496&v=2118014580');
+
+$product1->categorys->setIcon();
+
 var_dump($product1);
+var_dump($product1->categorys->icon);
 
 $product2 = new Food(2881,'croccantini','animal&co', new Category('gatti','icon'),25, 'url', 15, 'pesce', 'salmone');
+
+$product2->categorys->setIcon();
+
 var_dump($product2);
+var_dump($product2->categorys->icon);
+
 
 $product3 = new Toy(2881,'frisbee','animal&go', new Category('cani','icon'),17, 'url','gioco da lancio',20);
 var_dump($product3);
