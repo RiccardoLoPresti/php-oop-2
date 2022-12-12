3711cb4d97cae93b786e76905e3328cbaa5be209
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-12-12 14:05:09 +0100

    [add] diffenrent product

--- index.php
+++ index.php
@@ -7,7 +7,14 @@ require_once __DIR__ . './Models/Gadget.php';
 require_once __DIR__ . './Models/Toy.php';
 
 $product1 = new Product(12150,'mangiare', 'animalix',new Category('cani','icona'),30, 'https://medias.ultrapremiumdirect.com/italy/production/catalog/products/001005/1.jpg?tr=w-496,h-496&v=2118014580');
-
 var_dump($product1);
 
+$product2 = new Food(2881,'croccantini','animal&co', new Category('gatti','icon'),25, 'url', 15, 'pesce', 'salmone');
+var_dump($product2);
+
+$product3 = new Toy(2881,'frisbee','animal&go', new Category('cani','icon'),17, 'url','gioco da lancio',20);
+var_dump($product3);
+
+$product4 = new Gadget(2881,'Cuccia','animal&go', new Category('gatti','icon'),210, 'url','Alcantara',40);
+var_dump($product4);
 ?>
